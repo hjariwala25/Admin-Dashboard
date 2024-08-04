30180da09a928d795e2d65dd4c1cf84a80ea84eb
hjariwala25 <hjariwala150@gmail.com>
2024-08-04 21:08:38 +0530

    add

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-const SparkLine = () => {
+const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
+
   return (
-    <div>SparkLine</div>
+    <SparklineComponent
+    id={id}
+    height={height}
+    width={width}
+    lineWidth={1}
+    valueType="Numeric"
+    fill={color}
+    border={{ color: currentColor, }}
+    >
+
+    </SparklineComponent>
   )
+
 }
+
+
 
 export default SparkLine
