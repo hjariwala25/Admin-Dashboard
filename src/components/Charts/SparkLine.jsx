f0e5582f02a9033a8aa8f5ac9896366f7c4deb06
hjariwala25 <hjariwala150@gmail.com>
2024-08-06 19:33:29 +0530

    add

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -11,9 +11,21 @@ const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
     lineWidth={1}
     valueType="Numeric"
     fill={color}
-    border={{ color: currentColor, }}
+    border={{ color: currentColor, width: 2 }}
+    dataSource={data}
+    xName="x"
+    yName="y"
+    type={type}
+    tooltipSettings={{
+      visible: true,
+      format: '${x} : data ${y}',
+      trackLineSettings: {
+        visible: true
+      }
+    }}
     >
 
+    <Inject services={[SparklineTooltip]} />
     </SparklineComponent>
   )
 
