e4fbbd3562240fed2356faab0c52dccbc7386093
hjariwala25 <hjariwala150@gmail.com>
2024-08-16 14:34:32 +0530

    Fix: themeissue

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -9,7 +9,7 @@ const Orders = () => {
   const { currentMode } = useStateContext();
   const editing = { allowDeleting: true, allowEditing: true };
   return (
-    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white  rounded-3xl">
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 dark:bg-secondary-dark-bg bg-white  rounded-3xl">
       <Header category="Page" title="Orders" />
       <GridComponent
         id="gridcomp"
