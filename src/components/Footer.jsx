fa5b99a5db9eb8f17903a00bdfece0cc6f3374e4
hjariwala25 <hjariwala150@gmail.com>
2024-08-16 19:06:19 +0530

    Add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Footer = () => (
   <div className="mt-24">
     <p className="dark:text-gray-200 text-gray-700 text-center m-20">
-      © 2024 All rights reserved by Simplekart.com
+      © 2024 All rights reserved by 
+       <Link to = 'https://e-com-client-coral.vercel.app/' className='hover:text-blue-500 '> Simplekart.com</Link>
+      
     </p>
   </div>
 );
